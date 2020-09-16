818bd3a50d431d94a3625e284482f00b8d8d9f3d
nicolas gomez <nicolasg1911@gmail.com>
2020-09-15 23:13:08 -0500

    Cambio2

--- src/ui/TareaIntegradora1.java
+++ src/ui/TareaIntegradora1.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
 public class TareaIntegradora1{
-	public static final double OBRASBLANCAS=2600000;
-	public static final double OBRASNEGRAS=1300000;
-	public static final double PINTURA=1300000;
+	public static final double PLUMBINGWORK=2600000;
+	public static final double CONSTRUCTION=1300000;
+	public static final double PAINT=1300000;
 	public static void main(String[] args){
 		Scanner entry=new Scanner(System.in);
 		System.out.println("ingrese cantidad total de productos (no contar productos repetidos)");
@@ -12,11 +12,14 @@ public class TareaIntegradora1{
 		double [] barrioPrices=new double[totalItems];
 		double [] centroPrices=new double[totalItems];
 		int [] amountOfItems=new int[totalItems];
-		String [] useType=new String[totalItems];
-		
+		Object [] useType=new Object[totalItems];
+		boolean PWVerification=false;
+		boolean ConsVerification=false;
+		boolean PaintVerification=false;
+		entry.nextLine();
 		for(int i=0;i<totalItems;i++){
 			System.out.println("ingrese nombre del producto:");
-			names[i]=entry.next();
+			names[i]=entry.nextLine();
 			System.out.println("ingrese cantidad del producto:");
 			amountOfItems[i]=entry.nextInt();
 			System.out.println("ingrese precio en Home Center:");
@@ -25,11 +28,66 @@ public class TareaIntegradora1{
 			centroPrices[i]=entry.nextDouble();
 			System.out.println("ingrese precio en la ferreteria del barrio");
 			barrioPrices[i]=entry.nextDouble();
-			System.out.println("ingrese el uso que se le va a dar al material:");
-			useType[i]=entry.next();
+			entry.nextLine();
+			/*System.out.println("ingrese el uso que se le va a dar al material (obras blancas, obras negras o pintura):");
+			useType[i]=entry.nextLine();
 			useType[i]=useType[i].toLowerCase();
+			*/
 			}
+			for(int j=0;j<totalItems;j++){
+			System.out.println("ingrese el uso que se le va a dar al producto"+names[j]);
+			System.out.println("ingrese 1 para obras negras, 2 para obras blancas y 3 para pintura");
+			int typeDefinition=entry.nextInt();
+			switch(typeDefinition){
+				case 1:
+				useType[j]=UseTypeEnum.PLUMBINGWORK;
+				break;
+				case 2:
+				useType[j]=UseTypeEnum.CONSTRUCTION;
+				break;
+				case 3:
+				useType[j]=UseTypeEnum.PAINT;
+				break;
+			}
+			}
+			System.out.println("El costo total en Home Center es: "+TotalMetodHC(amountOfItems,HCPrices,totalItems,totalItems,PaintVerification,ConsVerification,PWVerification,useType));
+			System.out.println("El costo total en la ferreteria del barrio es: "+totalMetodBarrio(amountOfItems,barrioPrices,totalItems));
+			System.out.println("El costo total en la ferreteria del centro es: "+totalMetodCentro(amountOfItems,centroPrices,totalItems));
 		System.out.println("");
 	}
-	
+	public static double TotalMetodHC(int[] amountOfItems, double[] HCPrices, int totalItems, int totalItems2, boolean PaintVerification, boolean ConsVerification, boolean PWVerification,Object[] useType){
+		double totalPriceHC=0;
+		for(int i=0;i<totalItems;i++){
+			totalPriceHC=(amountOfItems[i]*HCPrices[i])+totalPriceHC;
+		}
+		for(int n=0;n<totalItems;n++){
+			if(useType[n]==UseTypeEnum.PLUMBINGWORK){
+				PWVerification=true;
+			}
+			if(useType[n]==UseTypeEnum.CONSTRUCTION){
+				ConsVerification=true;
+			}
+			if(useType[n]==UseTypeEnum.PAINT){
+				PaintVerification=true;
+			}
+		}
+		if((PaintVerification=true) && (ConsVerification=true) && (PWVerification=true)){
+			totalPriceHC=totalPriceHC+PLUMBINGWORK+CONSTRUCTION+PAINT;
+		}
+		return totalPriceHC;
+	}
+	public static double totalMetodCentro(int[] amountOfItems, double[] centroPrices, int totalItems){
+	double totalPriceCentro=0;
+		for(int i=0;i<totalItems;i++){
+			totalPriceCentro=(amountOfItems[i]*centroPrices[i])+totalPriceCentro;
+		}
+		return totalPriceCentro;
+	}
+	public static double totalMetodBarrio(int[] amountOfItems, double[] barrioPrices, int totalItems){
+	double totalPriceBarrio=0;
+		for(int i=0;i<totalItems;i++){
+			totalPriceBarrio=(amountOfItems[i]*barrioPrices[i])+totalPriceBarrio;
+		}
+		return totalPriceBarrio;
+	}
 }
---
+++ src/ui/UseTypeEnum.java
@@ -0,0 +1,3 @@
+public enum UseTypeEnum{
+	PLUMBINGWORK, CONSTRUCTION, PAINT
+}
